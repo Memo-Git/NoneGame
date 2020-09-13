09553c38f150f0a4476117fe18d9f91ca39767f2
Memo <qq969051037@gmail.com>
2020-09-13 22:16:17 +0800

    第一次测试上传

---
+++ Assets/Scripts/TestScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("这是第一个项目");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
